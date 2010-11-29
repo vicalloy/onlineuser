d99b19f8935be0e084b960aa2edb81a6ecb9dd13
vicalloy <zbirder@gmail.com>
2010-11-29 16:19:49 +0800

    + setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+version = '0.8'
+
+LONG_DESCRIPTION = """
+"""
+
+setup(
+    name='django-onlineuser',
+    version=version,
+    description="A django app to count online users.",
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+    keywords='onlineusers,django',
+    author='vicalloy',
+    author_email='zbirder@gmail.com',
+    url='https://github.com/vicalloy/onlineuser/',
+    license='BSD',
+    packages=find_packages(),
+    package_data = {
+        'onlineuser': [
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
