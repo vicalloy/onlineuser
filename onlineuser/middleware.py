a3482021a6c099173f06485a491e6f1bc192bd60
vicalloy <zbirder@gmail.com>
2011-04-28 00:32:20 +0800

    + add migrations for db * modify Online models, add session_key * identify user by session

--- onlineuser/middleware.py
+++ onlineuser/middleware.py
@@ -3,10 +3,12 @@ from onlineuser.models import Online
 class OnlineUserMiddleware:
     def process_request(self, request):
         user = request.user
+        session_key = request.session.session_key
         if user.is_authenticated():
             o, created = Online.objects.get_or_create(user=user)
+            o.session_key = session_key
         else:
             ip=request.META['REMOTE_ADDR']
-            o, created = Online.objects.get_or_create(ident=ip)
-        if not created:
-            o.save()
+            o, created = Online.objects.get_or_create(session_key=session_key)
+            o.ident=ip
+        o.save()
---
+++ onlineuser/migrations/0002_auto__add_field_online_session_key__del_unique_online_ident.py
@@ -0,0 +1,77 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        db.clear_table('onlineuser_online')
+        
+        # Removing unique constraint on 'Online', fields ['ident']
+        db.delete_unique('onlineuser_online', ['ident'])
+
+        # Adding field 'Online.session_key'
+        db.add_column('onlineuser_online', 'session_key', self.gf('django.db.models.fields.CharField')(default='', unique=True, max_length=64), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Online.session_key'
+        db.delete_column('onlineuser_online', 'session_key')
+
+        # Adding unique constraint on 'Online', fields ['ident']
+        db.create_unique('onlineuser_online', ['ident'])
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'onlineuser.online': {
+            'Meta': {'object_name': 'Online'},
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ident': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'session_key': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64'}),
+            'updated_on': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'blank': 'True', 'related_name': "'online'", 'unique': 'True', 'null': 'True', 'to': "orm['auth.User']"})
+        }
+    }
+
+    complete_apps = ['onlineuser']
