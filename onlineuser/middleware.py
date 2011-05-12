02bc212aec5eec7aa892ef974ece563b0bf37b84
vicalloy <zbirder@gmail.com>
2011-05-13 02:24:37 +0800

    * bug fix for session_key * use ip to ident user

--- onlineuser/middleware.py
+++ onlineuser/middleware.py
@@ -3,12 +3,12 @@ from onlineuser.models import Online
 class OnlineUserMiddleware:
     def process_request(self, request):
         user = request.user
-        session_key = request.session.session_key
+        ip=request.META['REMOTE_ADDR']
         if user.is_authenticated():
             o, created = Online.objects.get_or_create(user=user)
-            o.session_key = session_key
+            o.session_key = ip
         else:
-            ip=request.META['REMOTE_ADDR']
-            o, created = Online.objects.get_or_create(session_key=session_key)
-            o.ident=ip
-        o.save()
+            o, created = Online.objects.get_or_create(ident=ip)
+            o.session_key = ip
+        if not created:
+            o.save()
